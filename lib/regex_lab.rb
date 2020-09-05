4a4845a1e0452a314a8d40f3831daf0b66d21855
Alla Kuzminova <alchik@gmail.com>
2020-09-05 01:34:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
   puts word.match(/\b(a|e|i|o|u)/)
-  word.match(/\b(a|e|i|o|u)/) != nil
+  puts word.match(/\b(a|e|i|o|u)/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
