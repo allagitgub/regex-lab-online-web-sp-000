862912580848419378808bf74781ce20f418722d
Alla Kuzminova <alchik@gmail.com>
2020-09-05 02:30:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/^\(*\+*[1-9]{0,3}\)*-*[1-9]{0,3}[-. /]*\(*[2-9]\d{2}\)*[-. /]*\d{3}[-. /]*\d{4} *e*x*t*\.* *\d{0,4}$/) != nil
+  phone.match(/^([0-9\(\)\/\+ \-]*)$/) != nil
 end
